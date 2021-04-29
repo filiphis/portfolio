9e27cc221a9a55585ed6f873e32210d147354a3c
Luiz Silveira <l.filiphis@hotmail.com>
2021-04-28 23:24:42 -0300

    :hammer: Criando script para adicionar os projetos no html  mais facilmente

---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+import {
+  projects
+} from '../../projects.js'
+
+const newProject =
+  `
+<li class="challenges__item challenges__card">
+<figure class="challenges__figure">
+  <img src="${projects[0].projectName}/design/desktop-preview.jpg" alt="" class="challenges__image">
+  <a class="challenges__link" href="${projects[0].projectName}/" target="_blank">Visite</a>
+</figure>
+
+<div class="challenges__content">
+  <h2 class="challenges__title">${projects[0].projectName}</h2>
+  <div class="challenges__tags">
+    <a href="https://github.com/filiphis/portfolio/tree/main/${projects[0].projectName}" target="_blank" class="challenges__tagItem--github"><img src="assets/images/github.svg" alt="Repositorio github"></a>
+    <div>
+      <span class="challenges__tagItem challenges__tagItem--html">HTML</span>
+      <span class="challenges__tagItem challenges__tagItem--css">CSS</span>
+      <span class="challenges__tagItem challenges__tagItem--bem">BEM</span>
+    </div>
+  </div>
+</li>
+`;
+
+const lista = document.querySelector('.challenges__list');
+projects.forEach(project => {
+
+  let tecs = '';
+
+  if (project.Technologies && project.Technologies.length > 0) {
+    project.Technologies.forEach(tec => {
+      tecs += `<span class="challenges__tagItem challenges__tagItem--${tec}">${tec}</span>`
+    });
+  }
+
+  const newProject =
+    `
+  <li class="challenges__item challenges__card">
+  <figure class="challenges__figure">
+    <img src="${project.projectName}/design/desktop-preview.jpg" alt="" class="challenges__image">
+    <a class="challenges__link" href="${project.projectName}/" target="_blank">Visite</a>
+  </figure>
+
+  <div class="challenges__content">
+    <h2 class="challenges__title">${project.projectName}</h2>
+    <div class="challenges__tags">
+      <a href="https://github.com/filiphis/portfolio/tree/main/${project.projectName}" target="_blank" class="challenges__tagItem--github"><img src="assets/images/github.svg" alt="Repositorio github"></a>
+      <div>
+        ${tecs}
+      </div>
+    </div>
+  </li>
+  `;
+
+  lista.innerHTML += newProject;
+});
---
+++ projects.js
@@ -0,0 +1,58 @@
+export const projects = [{
+    id: 1,
+    projectName: '3-column-preview-card-component',
+    Technologies: ['html', 'css', 'sass', 'bem'],
+    difficulty: 'newbie',
+  },
+  {
+    id: 2,
+    projectName: 'profile-card-component',
+    Technologies: ['html', 'css', 'sass', 'bem'],
+    difficulty: 'newbie',
+  },
+  {
+    id: 3,
+    projectName: 'stats-preview-card-component',
+    Technologies: ['html', 'css', 'sass', 'bem'],
+    difficulty: 'newbie',
+  },
+  {
+    id: 4,
+    projectName: 'single-price-grid-component',
+    Technologies: ['html', 'css', 'bem'],
+    difficulty: 'newbie',
+  },
+  {
+    id: 5,
+    projectName: 'faq-accordion-card',
+    Technologies: ['html', 'css', 'sass', 'bem'],
+    difficulty: 'newbie',
+  },
+  {
+    id: 6,
+    projectName: 'social-proof-section',
+    Technologies: ['html', 'css', 'sass', 'bem'],
+    difficulty: 'newbie',
+  },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  {
+    id: 20,
+    projectName: 'fylo-landing-page-with-two-column-layout-master',
+    Technologies: ['html', 'css', 'bem'],
+    difficulty: 'junior',
+  },
+
+];
